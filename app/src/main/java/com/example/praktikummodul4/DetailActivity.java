3164f9a83329a833b755a8604e417e402ea3c33a
nandapril27 <nandapril27@gmail.com>
2024-04-07 14:28:04 +0700

    first

---
+++ app/src/main/java/com/example/praktikummodul4/DetailActivity.java
@@ -0,0 +1,27 @@
+package com.example.praktikummodul4;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class DetailActivity  extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        // Mendapatkan Intent yang memulai aktivitas ini
+        Intent intent = getIntent();
+
+        // Mendapatkan data tambahan dari Intent
+        String nama = intent.getStringExtra("key_nama");
+        int umur = intent.getIntExtra("key_umur", 0); // 0 adalah nilai default jika data tidak ditemukan
+
+        // Menampilkan data tambahan di TextView
+        TextView textView = findViewById(R.id.text_view);
+        textView.setText("Nama: " + nama + ", Umur: " + umur);
+    }
+}
